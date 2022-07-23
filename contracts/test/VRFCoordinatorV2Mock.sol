77089a24d5b1004fe6c1afd5a7cf325c23f7445c
Maakai123 <maakaiprincewill@gmail.com>
2022-07-23 14:04:19 +0100

    first commit

---
+++ contracts/test/VRFCoordinatorV2Mock.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@chainlink/contracts/src/v0.8/mocks/VRFCoordinatorV2Mock.sol";
+
+//VRFCoordinator constructor takes in _baseFee _gasPricelink
+//each request for Random number on Rinkeby will cost 0.26 lINK
---
+++ deploy/00-deploy-mocks.js
@@ -0,0 +1,36 @@
+
+ const { developmentChains } = require("../helper-hardhat-config")
+
+ //VRFCoordinator constructor takes in _baseFee _gasPricelink
+//each request for Random number on Rinkeby will cost 0.26 lINK
+
+const BASE_FEE = ethers.utils.parseEther("0.25") // 
+const GAS_PRICE_LINK = 1e9 // CALCULATED VALUE ON THE GAS PRICE OF THE CHAINS
+
+
+
+module.exports = async function ({ getNamedAccounts, deployments }){
+     
+    const { deploy, log } = deployments
+    const { deployer }  = await getNamedAccounts()
+    const args = [BASE_FEE, GAS_PRICE_LINK]
+
+    if (developmentChains.includes(network.name)) {
+         log("Local network detected! Deploying mocks...")
+         //deploy a mock vrfcoordinator...
+         await deploy ("VRFCoordinatorV2Mock", {
+               from: deployer,
+               log: true,
+               args: args,
+
+         })
+         log("Mocks Deployed")
+         log("..................................")
+         
+    }
+
+    
+
+}
+
+module.exports.tags = ["all", "mocks"]
